6e115db63782ba5c020b187171d9104c4b2cdaef
Davi1403 <davi.santos01010@gmail.com>
2026-05-16 18:11:55 -0300

    feat: start BackpackService

--- service/BackpackService.java
+++ service/BackpackService.java
@@ -1,6 +1,14 @@
 package service;
 
+import model.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BackpackService {
 
-    // 
+    public List<Player> team = new ArrayList<>();
+    while (team.size()) <= 11){
+
+    }
 }
